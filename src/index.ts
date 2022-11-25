1a4655768e1a0d76b70d7c593f0682dd0172fac0
Quentin Mendel <q.mendel@boeki.fr>
2022-11-25 16:27:26 +0100

    fix: add dist/ folder to build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import "./styles/app.scss";
-
 export * from "./components/interactive/ButtonComponent";
 export * from "./components/form/input/InputComponent";
 export * from "./components/form/select/SelectComponent";
